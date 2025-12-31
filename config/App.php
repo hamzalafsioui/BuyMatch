7149e20d360bb157492c7816f2b312c1b5c94065
hamzalafsioui <hamzalafsioui@gmail.com>
2025-12-31 23:06:44 +0100

    app.php

---
+++ config/App.php
@@ -0,0 +1,15 @@
+<?php
+// start session if not started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Include autoloader for automatic class loading
+require_once __DIR__ . '/autoload.php';
+
+// Database credentials
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'Sa@123456');
+define('DB_NAME', 'buyMatch_db');
+
