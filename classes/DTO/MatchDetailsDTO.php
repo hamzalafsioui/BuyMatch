713349316a557cefe474f98e24943430865f52ef
hamzalafsioui <hamzalafsioui@gmail.com>
2026-01-06 10:02:36 +0100

    MatchDetailsDTO

---
+++ classes/DTO/MatchDetailsDTO.php
@@ -0,0 +1,17 @@
+<?php
+
+class MatchDetailsDTO
+{
+
+    public function __construct(
+        public int $matchId,
+        public string $homeTeamName,
+        public string $homeTeamLogo,
+        public string $awayTeamName,
+        public string $awayTeamLogo,
+        public string $venueName,
+        public string $venueCity,
+        public string $matchDatetime,
+        public string $status
+    ) {}
+}
