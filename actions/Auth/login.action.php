66c47f6308f2a66121f9e5e0095933cb3299f533
hamzalafsioui <hamzalafsioui@gmail.com>
2026-01-06 14:25:42 +0100

    add Redirection

--- actions/Auth/login.action.php
+++ actions/Auth/login.action.php
@@ -20,13 +20,27 @@ if (Auth::login($email, $password)) {
     $currentUser = Auth::getCurrentUser();
     $redirect = BASE_URL . '/index.php'; // Default
 
-    if ($currentUser) {
-        if ($currentUser->getRoleId() == 2) { // Organizer
-            $redirect = BASE_URL . '/pages/organizer/dashboard.php';
-        } elseif ($currentUser->getRoleId() == 3) { // Admin
-            $redirect = BASE_URL . '/pages/admin/dashboard.php';
+    // Redirection
+    $redirectionPara = $_POST["redirect"] ?? null;
+    if (!empty($redirectionPara)) {
+        $decodedRedirect = urldecode($redirectionPara);
+
+        // check is starts with BASE_URL or /
+        if (str_starts_with($decodedRedirect, 'http') 
+            || 
+        str_starts_with($decodedRedirect, "/")) {
+            $redirect = $decodedRedirect;
+        } 
+    } else {
+        if ($currentUser) {
+            if ($currentUser->getRoleId() == 2) { // Organizer
+                $redirect = BASE_URL . '/pages/organizer/dashboard.php';
+            } elseif ($currentUser->getRoleId() == 3) { // Admin
+                $redirect = BASE_URL . '/pages/admin/dashboard.php';
+            }
         }
     }
+
 
     echo json_encode(['success' => true, 'message' => 'Login successful', 'redirect' => $redirect]);
 } else {
