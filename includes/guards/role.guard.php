a629dfce42be61abdc6eb3972a1925bf00374f6e
hamzalafsioui <hamzalafsioui@gmail.com>
2026-01-09 21:18:39 +0100

    change requireRole to accept multiple role Ids and add check for organizers

--- includes/guards/role.guard.php
+++ includes/guards/role.guard.php
@@ -2,12 +2,22 @@
 
 require_once __DIR__ . '/../../config/App.php';
 
-function requireRole(int $roleId): void
+function requireRole(int ...$roleIds): void
 {
     $user = Auth::getCurrentUser();
 
-    if (!$user || $user->getRoleId() !== $roleId) {
-        header('Location: /pages/auth/login.php');
+    if (!$user || !in_array($user->getRoleId(), $roleIds)) {
+        header('Location: ' . BASE_URL . '/pages/auth/login.php');
         exit;
+    }
+
+    // check for Organizers
+    if ($user->getRoleId() == 2) {
+        $currentPage = $_SERVER['PHP_SELF'];
+        // If not accepted and not already on the pending page
+        if (!$user->isAcceptable() && strpos($currentPage, 'pending_approval.php') === false) {
+            header('Location: ' . BASE_URL . '/pages/auth/pending_approval.php');
+            exit;
+        }
     }
 }
